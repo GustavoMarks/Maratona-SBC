ec7745954002498dc1b83c4cb20dbec96c064c37
GustavoM7 <gustavosmarquesf@gmail.com>
2019-09-10 20:36:17 -0300

    melhorando merge sort

--- C++/Algoritmos/Ordenação/Mergesort.hpp
+++ C++/Algoritmos/Ordenação/Mergesort.hpp
@@ -1,50 +1,37 @@
-#ifndef MERGESERT_HPP
-#define MERGESERT_HPP
+#ifndef MERGESORT_HPP
+#define MERGESORT_HPP
 
-void merge(int A[], int p, int q, int r){
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    int L[n1];
-    int R[n2];
-    for(int i= 0; i < n1; i++){
-        L[i] = A[p + i];
-    }
-    for(int j= 0; j < n2; j++){
-        R[j] = A[q + j + 1];
+//complexidade O(nlogn)
+
+void merge(int vetor[], int vetorAux[], int comeco, int meio, int fim){
+    int esq, dir;
+    esq = comeco;
+    dir = meio;
+
+    for(int i=comeco; i < fim; i++){
+        if(esq < meio && (dir >= fim || vetor[esq] < vetor[dir])){
+            vetorAux[i] = vetor[esq];
+            esq++;
+        } else {
+            vetorAux[i] = vetor[dir];
+            dir++;
+        }
     }
 
-    int i = 0;
-    int j = 0;
-    int k = p;
-    while (i < n1 && j < n2){ 
-        if (L[i] >= R[j]){ 
-            A[k] = L[i]; 
-            i++; 
-        } else{ 
-            A[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-    while (i < n1){ 
-        A[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-    while (j < n2){ 
-        A[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
+    for(int i = comeco; i < fim; i++){
+        vetor[i] = vetorAux[i];
+    }
+
 }
 
-void merge_sort(int A[], int p, int r){
-    if(p < r){
-        int q = (p + r)/2;
-        merge_sort(A, p, q);
-        merge_sort(A, q + 1, r);
-        merge(A, p, q, r);
-    }
+
+void mergeSort(int vetor[], int vetorAux[], int comeco, int fim){
+    if(fim - comeco < 2) return;
+
+    int meio = (comeco + fim)/2;
+    mergeSort(vetor, vetorAux, comeco, meio);
+    mergeSort(vetor, vetorAux, meio, fim);
+    merge(vetor, vetorAux, comeco, meio, fim);
 }
 
 #endif
